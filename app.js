9f6aa6e64f0420041b09f07d2b3066a8f2de8a2b
Jayesh Jain <jjayesh.500@gmail.com>
2023-05-16 21:15:27 +0530

    MyBookList_App

---
+++ app.js
@@ -0,0 +1,176 @@
+// Book Class : Represent a Book 
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+
+    static addBookToList(book) {
+
+        const list = document.getElementById('book-list');
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+           <td>${book.title}</td>
+           <td>${book.author}</td>
+           <td>${book.isbn}</td>
+           <td><a href = "#" class = "btn btn-danger btn-sm delete" >X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        //Vanish after 3 seconds
+
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
+    static clearFields() {
+
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+
+    }
+}
+
+// Store Class: Handle Storage
+
+class Store{
+
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static addBooks(book){ 
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn){
+
+        const books = Store.getBooks();
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+}
+
+
+
+// Event Display Books
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent Default Submit
+    e.preventDefault();
+
+    //Get Form Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Validate
+
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please Fill All Fields.!','danger');
+    } else {
+
+        // Instantiate Book
+
+        const book = new Book(title, author, isbn);
+
+        console.log(book);
+
+        // Add Book To UI
+
+        UI.addBookToList(book);
+
+        //Add Book To Store
+
+        Store.addBooks(book);
+
+        // Show Success Message
+
+        UI.showAlert('Book Added Successfully.!','success');
+
+        // Add Book To UI
+
+        UI.clearFields();
+    }
+
+
+
+
+})
+
+// Event: Add a Book
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    if(confirm('Are You Sure you want to delete this?') === true){
+
+        // Remove book from UI
+
+        UI.deleteBook(e.target);
+
+        // Remove book from store
+
+        Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+        // Show Success Message
+
+        UI.showAlert('Book Deleted Successfully.!','success');
+
+    }else{
+
+        // Show Success Message
+
+        UI.showAlert('No changes were made.!','danger');
+
+    }
+})
